5cfe0e587ae43419f3be193a5984cd259e1e8084
Tim Costermans <tim.costermans@student.ap.be>
2025-10-19 20:00:28 +0200

    generate a mission on game load, added pallet prefabs

---
+++ PalletPanic/Assets/Scripts/Pallet.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public enum PalletColor
+{
+    Red,
+    Blue,
+    Green,
+    Yellow
+}
+
+public class Pallet : MonoBehaviour
+{
+    public PalletColor palletColor;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
