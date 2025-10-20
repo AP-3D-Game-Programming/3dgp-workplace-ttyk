4edf4597e585808cde5389d32b65517ecd93ea66
Tim Costermans <tim.costermans@student.ap.be>
2025-10-20 21:39:29 +0200

    Refactor project structure

--- PalletPanic/Assets/Scripts/Pallet.cs
+++ PalletPanic/Assets/Scripts/Pallet.cs
@@ -12,15 +12,66 @@ public class Pallet : MonoBehaviour
 {
     public PalletColor palletColor;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public Material redMaterial;
+    public Material blueMaterial;
+    public Material greenMaterial;
+    public Material yellowMaterial;
 
+    private Renderer palletRenderer;
+
+    private void InitializeRenderer()
+    {
+        if(palletRenderer == null)
+        {
+            palletRenderer = GetComponent<Renderer>();
+            if (palletRenderer == null)
+            {
+                palletRenderer = GetComponentInChildren<Renderer>();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        InitializeRenderer();
+        ApplyMaterial();
+    }
 
+    private void OnValidate() //wijzigingen via inspector
+    {
+        InitializeRenderer();
+        ApplyMaterial();
+    }
+
+    public void SetColor(PalletColor newColor)
+    {
+        palletColor = newColor;
+        ApplyMaterial();
+    }
+
+    private void ApplyMaterial()
+    {
+        Material materialToApply = GetMaterialForColor(palletColor);
+
+        if (materialToApply != null && palletRenderer != null)
+        {
+            palletRenderer.material = materialToApply;
+        }
+    }
+    private Material GetMaterialForColor(PalletColor color)
+    {
+        switch (color)
+        {
+            case PalletColor.Red:
+                return redMaterial;
+            case PalletColor.Blue:
+                return blueMaterial;
+            case PalletColor.Green:
+                return greenMaterial;
+            case PalletColor.Yellow:
+                return yellowMaterial;
+            default:
+                return null;
+        }
     }
 }
