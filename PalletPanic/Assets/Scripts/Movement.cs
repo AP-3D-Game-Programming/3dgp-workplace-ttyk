bcd69b76bc8190628b90affa304ec2f7c20bc83b
zeus <you@example.com>
2025-11-16 12:18:54 +0100

    added score and test with P

--- PalletPanic/Assets/Scripts/Movement.cs
+++ PalletPanic/Assets/Scripts/Movement.cs
@@ -60,6 +60,15 @@ public class Movement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
+        // TEST: Druk P om score te verhogen
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (ScoreManager.Instance != null)
+            {
+                ScoreManager.Instance.AddScore(10);
+            }
+        }
+
         //move the vehicle forward
         float targetSpeed = speed * forwardInput;
         if (Mathf.Abs(forwardInput) > INPUT_THRESHOLD)
